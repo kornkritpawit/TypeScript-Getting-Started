e492043992913edf662f18355fc55acab196f36c
Kornkritpawit <konsoo@hotmail.com>
2021-01-29 11:57:46 +0700

    finish module2

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,26 @@
 function startGame() {
 // strting a new game
+
+  // const playerName: string = 'Audrey';
+  let playerName: string | null;
+  playerName = null;
+  logPlayer(playerName);
+
+  // var messagesElement: HTMLElement | string;
+  // messagesElement = document.getElementById('messages');
+  // if (typeof messagesElement === 'string') {
+  //   return messagesElement;
+  // }
+  // else {
+  //   return messagesElement;
+  // }
+
   var messagesElement = document.getElementById('messages');
   // messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name: string | null) {
+  console.log(`New game starting for player: ${name}`);
 }
 document.getElementById('startGame')!.addEventListener('click', startGame)
