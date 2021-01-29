3cbcbafaac2573ef7423e171c0ae2717e934e6b0
Kornkritpawit <konsoo@hotmail.com>
2021-01-29 13:46:30 +0700

    finish module 4

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,9 @@ function startGame() {
 // strting a new game
 
   // const playerName: string = 'Audrey';
-  let playerName: string | null;
-  playerName = null;
+  let playerName: string | undefined = getInputValue('playername');
+  // playerName = 'Throne';
+
   logPlayer(playerName);
 
   // var messagesElement: HTMLElement | string;
@@ -15,12 +16,53 @@ function startGame() {
   //   return messagesElement;
   // }
 
-  var messagesElement = document.getElementById('messages');
-  // messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+  // var messagesElement = document.getElementById('messages');
+  // // messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+  // messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+
+  postScore(100, playerName)
+  // postScore(80)
+  postScore(-5, playerName);
 }
 
-function logPlayer(name: string | null) {
+function logPlayer(name?: string ): void {
   console.log(`New game starting for player: ${name}`);
 }
+
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+  
+  if (inputElement.value == '') {
+    return undefined;
+  }
+  else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+  let logger: (value: string) => void;
+
+  if (score < 0 ) {
+    logger = logError;
+  } else {
+    logger = logMessage; 
+  }
+
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} = ${playerName}`;
+
+  logger(`Score: ${score}`);
+}
 document.getElementById('startGame')!.addEventListener('click', startGame)
+
+// const logMessage = (message: string): void => console.log(message);
+const logMessage = (message: string) => console.log(message);
+
+// logMessage('Welcome to Multimath!');
+
+function logError(err: string): void {
+  console.error(err);
+}
+
